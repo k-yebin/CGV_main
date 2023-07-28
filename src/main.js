b6db5bd74d903c9ef4d265d7f60deff6842b2cef
mss3662352 <133845158+k-yebin@users.noreply.github.com>
2023-07-28 20:14:22 +0900

    최종

--- src/main.js
+++ src/main.js
@@ -30,17 +30,6 @@ $(window).scroll(function () {
     }
 })
 
-//무비차트 탭메뉴
-//첫번째 탭
-$(function () {
-    $('ul.movie_tab li').click(function () {
-        var activeTab = $(this).attr('data-tab');
-        $('ul.movie_tab li').removeClass('on');
-        $('.tabcont').removeClass('on');
-        $(this).addClass('on');
-        $('#' + activeTab).addClass('on');
-    })
-});
 
 //메인 비주얼
 $('.visual_slick').slick({
@@ -51,6 +40,56 @@ $('.visual_slick').slick({
     arrows: false,
     autoplay: true,
     fade: true
+});
+
+//무비차트 탭메뉴
+$(document).ready(function () {
+    $('.movie_tab li').click(function () {
+        var activeTab = $(this).attr('data-tab');
+        $('.movie_tab li').removeClass('active');
+        $('.tab_content').removeClass('active');
+        $(this).addClass('active');
+        $('#' + activeTab).addClass('active');
+    });
+});
+
+//무비차트 슬라이더
+$('.movie_slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 800,
+    prevArrow: $('.prev02'),
+    nextArrow: $('.next02')
+});
+
+$('.movie_slider02').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 800,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next')
+});
+
+//모바일 무비차트 탭메뉴
+const tabs = document.querySelectorAll('.mobile_tab');
+const tabContents = document.querySelectorAll('.mobile_tab-content');
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+        // 모든 탭과 탭 컨텐츠 비활성화
+        tabs.forEach((t) => t.classList.remove('active'));
+        tabContents.forEach((content) => content.classList.remove('active'));
+
+        // 클릭한 탭과 해당 탭 컨텐츠 활성화
+        tab.classList.add('active');
+        const activeTab = tab.getAttribute('data-tab');
+        const activeContent = document.getElementById(activeTab);
+        activeContent.classList.add('active');
+    });
 });
 
 //event_middle_banner
@@ -65,14 +104,29 @@ $('.event_middle_banner').slick({
 
 //아코디언메뉴
 $(function () {
-    $('.accordian-item .content').eq(0).show() //콘텐츠 하나는 펼쳐진채로 보여줌
+    // 페이지가 로드되면 첫 번째 콘텐츠를 열도록 설정
+    $('.accordian-item .content').eq(0).addClass('active');
+
     $('.accordian-item .title').click(function () {
-        $(this).next().stop().slideToggle(300)
-        $(this).toggleClass('active')
-        //this = 함수적용된 애만 메서드가 적용됨. 
-        //this를 기준으로 next에 메서드 적용해라.
-    })
-})
+        const content = $(this).next();
+        // 클릭한 콘텐츠를 열고 나머지 콘텐츠는 닫음
+        $('.accordian-item .content').not(content).removeClass('active').slideUp(300);
+        content.stop().slideToggle(300);
+        $(this).toggleClass('active');
+    });
+});
+
+//모바일 푸터 아코디언
+$(function () {
+    $('.inform_title').click(function () {
+        const informInner = $(this).next('.inform_inner');
+        if (informInner.height() === 0) {
+            informInner.animate({ height: '150px' }, 300);
+        } else {
+            informInner.animate({ height: '0' }, 300);
+        }
+    });
+});
 
 
 /* 탑버튼 */
